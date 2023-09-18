7eaeec6c5e6bc0e6e41d9b5e85de84e9315d175a
cc <44751363+zzfn@users.noreply.github.com>
2023-09-18 20:12:07 +0800

    feat: demo

--- src/main.rs
+++ src/main.rs
@@ -6,67 +6,55 @@ use swc_common::{
 use swc_common::sync::Lrc;
 use swc_ecma_parser::{Parser, StringInput, Syntax, TsConfig};
 use std::fs;
-use std::path::Path;
 use std::io::{self, Write};
+use std::path::PathBuf;
 use swc_ecma_visit::Visit;
 use visitor::{scorer::Scorer, import_visitor::ImportVisitor};
+fn to_absolute_path(relative_path: &str) -> PathBuf {
+    let current_dir = std::env::current_dir().expect("Failed to get current directory");
+    current_dir.join(relative_path)
+}
 fn main() {
     print!("Please enter the path to the input file: ");
-    io::stdout().flush().unwrap(); // 使得上面的print!立即显示，不需要等待换行
+    io::stdout().flush().unwrap();
 
     let mut path_to_file = String::new();
     io::stdin().read_line(&mut path_to_file).expect("Failed to read line");
-    let path_to_file = path_to_file.trim(); // 移除任何的尾随换行或空格
-
-    let source_code = match fs::read_to_string(Path::new(&path_to_file)) {
-        Ok(contents) => contents,
-        Err(err) => {
-            eprintln!("Error reading file: {:?}", err);
-            return;
-        }
-    };
-
+    let path_to_file = path_to_file.trim();
+    let abs_path = to_absolute_path(path_to_file);
+    println!("Absolute path: {}", abs_path.display());
     let cm: Lrc<SourceMap> = Default::default();
 
-    let fm = cm.new_source_file(
-        FileName::Custom(path_to_file.to_string()),
-        source_code,
-    );
-
-    // 创建解析器
-    let lexer = swc_ecma_parser::lexer::Lexer::new(
-        Syntax::Typescript(TsConfig {
-            tsx: true,
-            decorators: true,
-            dts: false,
-            no_early_errors: false,
-            disallow_ambiguous_jsx_like: false,
-        }),
-        swc_ecma_ast::EsVersion::EsNext,
-        StringInput::from(&*fm),
-        None,
-    );
-
-    let mut parser = Parser::new_from(lexer);
-
-    // 解析源代码
-    match parser.parse_module() {
-        Ok(module) => {
-            println!("{:#?}", module);
-            let mut visitor = ImportVisitor::new();
-            visitor.visit_module(&module);
-
-            for import in &visitor.imports {
-                println!("Imported: {}", import);
-            }
-            let mut scorer = Scorer::new();
-            scorer.visit_module(&module);
-
-            println!("Score: {}", scorer.score);
-        }
-        Err(err) => {
-            eprintln!("Error: {:?}", err);
-        }
+    let mut visitor = ImportVisitor::new();
+    if let Err(error) = visitor.process_file(&cm, &path_to_file) {
+        eprintln!("Error processing file: {}", error);
+        return;
     }
 
+    for import in &visitor.imports {
+        println!("Imported: {}", import);
+    }
+
+    let mut scorer = Scorer::new();
+    for file in &visitor.visited_files {
+        let source_code = fs::read_to_string(file).expect("Failed to read file");
+        let fm = cm.new_source_file(FileName::Custom(file.to_string()), source_code);
+        let lexer = swc_ecma_parser::lexer::Lexer::new(
+            Syntax::Typescript(TsConfig {
+                tsx: true,
+                decorators: true,
+                dts: false,
+                no_early_errors: false,
+                disallow_ambiguous_jsx_like: false,
+            }),
+            swc_ecma_ast::EsVersion::EsNext,
+            StringInput::from(&*fm),
+            None,
+        );
+        let mut parser = Parser::new_from(lexer);
+        let module = parser.parse_module().unwrap();
+        scorer.visit_module(&module);
+    }
+
+    println!("Score: {}", scorer.score);
 }
--- src/visitor/import_visitor.rs
+++ src/visitor/import_visitor.rs
@@ -1,20 +1,94 @@
+use std::fs;
+use std::path::Path;
+use swc_common::input::StringInput;
+use swc_common::{FileName, SourceMap};
+use swc_common::sync::Lrc;
 // src/visitor/import_visitor.rs
 use swc_ecma_ast::*;
+use swc_ecma_parser::{Parser, Syntax, TsConfig};
 use swc_ecma_visit::Visit;
 
 pub struct ImportVisitor {
-    pub(crate) imports: Vec<String>,
+    pub imports: Vec<String>,
+    pub visited_files: Vec<String>,
 }
 
 
 impl ImportVisitor {
-    pub(crate) fn new() -> Self {
-        ImportVisitor { imports: vec![] }
+
+    pub fn new() -> Self {
+        ImportVisitor { imports: vec![], visited_files: vec![] }
+    }
+
+    pub(crate) fn process_file(&mut self, cm: &Lrc<SourceMap>, path: &str) -> Result<(), String> {
+        if self.visited_files.contains(&path.to_string()) {
+            return Ok(());
+        }
+
+        self.visited_files.push(path.to_string());
+
+        // 打印当前正在处理的文件路径
+        println!("Processing file: {}", path);
+
+        let source_code = fs::read_to_string(Path::new(path)).map_err(|e| format!("{:?}", e))?;
+
+        let fm = cm.new_source_file(FileName::Custom(path.to_string()), source_code);
+
+        // 创建解析器
+        let lexer = swc_ecma_parser::lexer::Lexer::new(
+            Syntax::Typescript(TsConfig {
+                tsx: true,
+                decorators: true,
+                dts: false,
+                no_early_errors: false,
+                disallow_ambiguous_jsx_like: false,
+            }),
+            swc_ecma_ast::EsVersion::EsNext,
+            StringInput::from(&*fm),
+            None,
+        );
+
+        let mut parser = Parser::new_from(lexer);
+
+        let module = parser.parse_module().map_err(|e| format!("{:?}", e))?;
+        self.visit_module(&module);
+
+        let current_imports = self.imports.clone();
+        for import in &current_imports {
+            if import.starts_with(".") {
+                println!("Processing import: {}", import);
+                self.process_file(cm, import)?;
+            }else {
+                println!("Skipping absolute import: {}", import);
+            }
+        }
+
+        Ok(())
+    }
+    fn try_resolve_module(&self, path: &str) -> Option<String> {
+        let possible_extensions = ["", ".js", ".jsx", ".ts", ".tsx", ".json", ".vue"];
+
+        for ext in &possible_extensions {
+            let full_path = format!("{}{}", path, ext);
+            println!("Trying path: {}", full_path);
+            if Path::new(&full_path).exists() {
+                return Some(full_path);
+            }
+        }
+
+        None
     }
 }
 
 impl Visit for ImportVisitor {
     fn visit_import_decl(&mut self, node: &ImportDecl) {
-        self.imports.push(node.src.value.to_string());
+        if let Some(resolved_path) = self.try_resolve_module(&node.src.value.to_string()) {
+            self.imports.push(resolved_path);
+        } else {
+            // 可选: 处理不能解析的模块导入，例如通过日志记录或其他方式
+            eprintln!("Failed to resolve module: {}", node.src.value);
+        }
     }
 }
+
+// src/visitor/scorer.rs
